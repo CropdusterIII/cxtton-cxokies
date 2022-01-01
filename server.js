d1a5ec8512124cbc476fc9afb7035c07b3d6b3dd
Glitch (cobalt-quaint-tilapia) <none>
2022-01-01 21:00:01 +0000

    🕋❄️ Checkpoint ./lib/definitions.js:25363807/289 ./server.js:25363807/12

--- server.js
+++ server.js
@@ -4740,13 +4740,13 @@ var maintainloop = (() => {
                     o.define(Class.bot) 
                     o.define(ran.choose([Class.basic, Class.basicp2]));
                     o.name += ran.chooseBotName();
-                    o.color = ran.choose([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55]) 
+                    o.color = ran.choose([/*0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,*/ 12, /*13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55*/]) 
                     o.refreshBodyAttributes(); 
-                    //  o.team = ran.chooseBot2Team();
+                    /*  o.team = ran.chooseBot2Team();
                     if (o.team === -1) {o.color = 10};
                     if (o.team === -2) {o.color = 11};
                     if (o.team === -3) {o.color = 12};
-                    if (o.team === -4) {o.color = 15};
+                    if (o.team === -4) {o.color = 15};*/
                     bots.push(o);
                 }
                   // Remove dead ones
